9293d936a633768898e1732dd24039fd7e805bbf
Papa Abeku <papaotis2010@gmail.com>
2024-06-04 17:26:18 +0000

    Addon

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
-export default function getListStudents() {
-  const res = [];
-  res.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
-  res.push({ id: 2, firstName: 'James', location: 'Columbia' });
-  res.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
-  
-  return res;
-  }
+const getListStudents = () => [
+	{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+	{ id: 2, firstName: 'James', location: 'Columbia' },
+	{ id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+	
+export default getListStudents;
