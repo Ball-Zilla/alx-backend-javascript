09bd63914cee2f7da6d86167e8b88f9ceeb182b9
Papa Abeku <papaotis2010@gmail.com>
2024-06-04 20:18:13 +0000

    Task 9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,10 @@
 export default function groceriesList() {
-  return new Map([
+  const values = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
-    ['Bananas', 5],
-  ]);
+    ['Banana', 5],
+  ];
+  return new Map(values);
 }
