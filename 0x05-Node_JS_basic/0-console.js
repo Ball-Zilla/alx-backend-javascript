4d25964aa18bdc18644ec70b782512d1209b4bde
Papa Abeku <papaotis2010@gmail.com>
2024-07-22 21:25:29 +0000

    Half Price

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Zilla <https://github.com/Ball-Zilla>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
